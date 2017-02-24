721b5e69dd19b811af60347c29bf58ac2d68a8a2
Marlon Aviz <aviz.marlon@gmail.com>
2017-02-24 18:34:49 -0300

    added clear_console module

--- tmx-combine-optimizer.py
+++ tmx-combine-optimizer.py
@@ -2,13 +2,15 @@ import re
 import os
 import tkinter
 from tkinter.filedialog import askdirectory
-
+from clear_console import clear_console
 
 root = tkinter.Tk().withdraw()
 path = askdirectory(title="Select the folder that contains .TMX files")
 tmx_extension = '.tmx'
 contents = os.walk(path)
 srclangs = {}
+
+clear_console()
 
 # look in all folders, subolders and files of each of these folders and subfolders
 for dirpath, dirname, filename in contents:
@@ -22,7 +24,7 @@ for dirpath, dirname, filename in contents:
 				if len(xcode) > 0:
 					for match in xcode:
 						line = line.replace(''.join(match), '') # removes the hext text code, leaving only the actual text
-
+				print("Searching line " + line, end='\r')
 				# if we find the line with srclang value, harvest it,
 				# put it into our srclangs database and jump to the next file in the loop
 				# otherwise, go to the next line and just keep searching for the srclang attribute
@@ -35,6 +37,7 @@ for dirpath, dirname, filename in contents:
 
 # checks if there is any srclang with a value different from 'EN-GB'
 # and prints it out for us to see
+clear_console()
 for srclang in srclangs:
 	if srclangs[srclang] != 'EN-GB':
 		print("This file diverged from the common srclang value: " + srclang)
